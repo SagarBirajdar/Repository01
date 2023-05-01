5ec43b59a9e326fe333627b9aa0e212c767db662
SagarBirajdar <birajdarsager1010@gmail.com>
2023-05-01 10:36:00 +0530

    Two applications are created

--- repo01.java
+++ repo01.java
@@ -1,3 +1,5 @@
 First repo file for practice..
 
 Important note is when we create new repository we need to add url and main to git bash and execute.
+
+second repo file is added..
---
+++ repo02.java
@@ -0,0 +1,1 @@
+Second Repo file is created.
